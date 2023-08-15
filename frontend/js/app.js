77b1adc36ed50e499156242a068d9ad1972d6a21
RoshensLove <alegatorwork@gmail.com>
2023-08-15 22:43:32 +0300

    project done

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,49 +1,86 @@
 const SERVER_URL = "http://localhost:3000";
 
- async function serverAddStudent() {
-    let respons = await fetch("http://localhost:3000")
+ async function serverAddStudent(obj) {
+    let response = await fetch(SERVER_URL + "/api/students", {
+        method:"POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(obj),
+    });
+
+    let data = await response.json()
+    return data
+
  }
 
+ async function serverGetStudent() {
+    let response = await fetch(SERVER_URL + "/api/students", {
+        method:"Get",
+        headers: { "Content-Type": "application/json" },
+    });
+
+    let data = await response.json()
+    return data
+
+ }
+
+ async function serverDeleteStudent(id) {
+    let response = await fetch(SERVER_URL + "/api/students/" + id, {
+        method:"DELETE",
+    });
+
+    let data = await response.json()
+    return data
+
+ }
+
+let serverData = await serverGetStudent()
 // let listStudents = [
 //     {
 //         name: "Олег",
-//         lastname: "Островский",
+//         surname: "Островский",
 //         middleName: "Русланович",
 //         birthday: new Date(2001, 1, 21),
 //         fuculty: "ФИСЕ",
-//         start: 2019,
+//         studyStart: 2019,
 //     },
 //     {
 //         name: "Петр",
-//         lastname: "Петриченко",
+//         surname: "Петриченко",
 //         middleName: "Олегович",
 //         birthday: new Date(2005, 6, 18),
 //         fuculty: "Экономика",
-//         start: 2021,
+//         studyStart: 2021,
 //     },
 //     {
 //         name: "Ольга",
-//         lastname: "Бурундукова",
+//         surname: "Бурундукова",
 //         middleName: "Александрова",
 //         birthday: new Date(2008, 6, 3),
 //         fuculty: "Информатика",
-//         start: 2010,
+//         studyStart: 2010,
 //     }
 // ]
 
 /////////////////////////////////
 
-function padTo2Digits(num) {
-    return num.toString().padStart(2, '0');
-  }
-  
-  function formatDate(date) {
-    return [
-      padTo2Digits(date.getDate()),
-      padTo2Digits(date.getMonth() + 1),
-      date.getFullYear(),
-    ].join('/');
-  }
+let listStudents = [];
+
+if(serverData) {
+    listStudents = serverData
+}
+
+function formatDate(date) {
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
+
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+    
+    let yy = date.getYear();
+    if (yy < 10) yy = '0' + yy;
+
+    return dd + '.' + mm + '.' + yy;
+}
 
 /////////////////////////////////
 
@@ -52,15 +89,27 @@ function $getNewStudentTR(studObj) {
     const $tr = document.createElement("tr");
     const $tdFIO = document.createElement("td");
     const $tdBirthday = document.createElement("td");
-    const $tdFaculty = document.createElement("td");
-    const $tdStart = document.createElement("td");
+    const $tdfaculty = document.createElement("td");
+    const $tdstudyStart = document.createElement("td");
+    const $tdDelete = document.createElement("td");
+    const $btnDelete = document.createElement("button");
 
-    $tdFIO.textContent = `${studObj.lastname} ${studObj.name} ${studObj.middleName}`
-    $tdBirthday.textContent = formatDate(studObj.birthday)
-    $tdFaculty.textContent = studObj.fuculty
-    $tdStart.textContent = studObj.start
 
-    $tr.append($tdFIO, $tdBirthday, $tdFaculty, $tdStart);
+    $btnDelete.classList.add("btn", "btn-danger", "w-100");
+    $btnDelete.textContent = "Delete"
+
+    $tdFIO.textContent = `${studObj.surname} ${studObj.name} ${studObj.middleName}`
+    $tdBirthday.textContent = formatDate(new Date(studObj.birthday))
+    $tdfaculty.textContent = studObj.faculty
+    $tdstudyStart.textContent = studObj.studyStart
+
+    $btnDelete.addEventListener("click",async function() {
+        await serverDeleteStudent(studObj.id);
+        $tr.remove()
+    })
+
+    $tdDelete.append($btnDelete)
+    $tr.append($tdFIO, $tdBirthday, $tdfaculty, $tdstudyStart, $tdDelete);
     return $tr
 }
   
@@ -82,18 +131,24 @@ render(listStudents)
 
 //Add students
 
-const addStudents = document.getElementById("add-form").addEventListener("submit", event => {
+const addStudents = document.getElementById("add-form").addEventListener("submit", async function(event) {
     event.preventDefault()
 
     let newStudentObj = {
         name: document.getElementById("name-inp").value,
-        lastname: document.getElementById("surname-inp").value,
-        middleName: document.getElementById("middleName-inp").value,
+        surname: document.getElementById("surname-inp").value,
+        lastname: document.getElementById("middleName-inp").value,
         birthday: new Date(document.getElementById("birthday-inp").value),
-        fuculty: document.getElementById("faculty-inp").value,
-        start: document.getElementById("start-inp").value,
+        faculty: document.getElementById("faculty-inp").value,
+        studyStart: document.getElementById("studyStart-inp").value,
     }
 
+    let serverDataObj = await serverAddStudent(newStudentObj)
+
+    serverDataObj.birthday = new Date(serverDataObj.birthday)
+
     listStudents.push(newStudentObj);
+
+    console.log(listStudents)
     render(listStudents)
 })
